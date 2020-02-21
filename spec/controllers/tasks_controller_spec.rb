053146705e750982c929dda03c361fc7011a8650
jcgdxn@hotmail.com <jcgdxn@hotmail.com>
2020-02-21 06:10:24 +0000

    cleared data entries, wrking on heroku suucessful upload

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe TasksController, type: :controller do
       expect(response).to have_http_status(:success)
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value['title']).to eq("Fix things")
-      expect(Task.last.title).to eq("Fix things")      
+      expect(Task.last.title).to eq("Fix things")
     end
   end
 
